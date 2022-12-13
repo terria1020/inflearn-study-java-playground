10cb7fb0d9b10633c4fcee846a526197435d3036
terria1020 <jaehan1346@gmail.com>
2022-12-13 22:35:34 +0900

    4, 5단 구현 - 값 입력 및 변수
    4단 구현 후 단 수를 입력받는 코드를 작성하여 5단 구현

--- src/googoodan/part2/NormalGooGoo.java
+++ src/googoodan/part2/NormalGooGoo.java
@@ -2,6 +2,8 @@ package googoodan.part2;
 
 import studyTemplate.Study;
 
+import java.util.Scanner;
+
 public class NormalGooGoo implements Study {
     @Override
     public void main(String[] args) {
@@ -27,5 +29,41 @@ public class NormalGooGoo implements Study {
         System.out.println("3 * 7 = " + (3 * 7));
         System.out.println("3 * 8 = " + (3 * 8));
         System.out.println("3 * 9 = " + (3 * 9));
+
+        //4단
+        int result = 4 * 1;
+        System.out.println("4 * 1 = " + result);
+        result = 4 * 2;
+        System.out.println("4 * 2 = " + result);
+        result = 4 * 3;
+        System.out.println("4 * 3 = " + result);
+        result = 4 * 4;
+        System.out.println("4 * 4 = " + result);
+        result = 4 * 5;
+        System.out.println("4 * 5 = " + result);
+        result = 4 * 6;
+        System.out.println("4 * 6 = " + result);
+        result = 4 * 7;
+        System.out.println("4 * 7 = " + result);
+        result = 4 * 8;
+        System.out.println("4 * 8 = " + result);
+        result = 4 * 9;
+        System.out.println("4 * 9 = " + result);
+
+        //입력 및 출력 (5를 입력받는다고 가정)
+        System.out.println("구구단 중 출력할 단 수는: ");
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        System.out.println(number + " * 1 = " + (number * 1));
+        System.out.println(number + " * 2 = " + (number * 2));
+        System.out.println(number + " * 3 = " + (number * 3));
+        System.out.println(number + " * 4 = " + (number * 4));
+        System.out.println(number + " * 5 = " + (number * 5));
+        System.out.println(number + " * 6 = " + (number * 6));
+        System.out.println(number + " * 7 = " + (number * 7));
+        System.out.println(number + " * 8 = " + (number * 8));
+        System.out.println(number + " * 9 = " + (number * 9));
     }
 }
