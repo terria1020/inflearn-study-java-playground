96efcfb23a57462c178428bf8f025ea2433dd9ec
terria1020 <jaehan1346@gmail.com>
2022-12-13 23:05:06 +0900

    2-4. gugudan 8, 9단 값 입력 및 조건문
    단 수 입력을 통한 구구단 출력으로 수정, 조건문을 사용한 입력 값 검사

--- src/googoodan/part2/NormalGooGoo.java
+++ src/googoodan/part2/NormalGooGoo.java
@@ -8,26 +8,24 @@ public class NormalGooGoo implements Study {
     @Override
     public void main(String[] args) {
 
-        int i = 1;
-
         //입력 및 출력
-        //System.out.println("구구단 중 출력할 단 수는: ");
-        //Scanner sc = new Scanner(System.in);
+        System.out.println("구구단 중 출력할 단 수는: ");
+        Scanner sc = new Scanner(System.in);
 
-        //int number = sc.nextInt();
-        int number = 6;
+        int number = sc.nextInt();
 
-        //6단, 반복문의 사용
-        while (i < 10) {
-            System.out.println(number + " * " + i + " = " + (number * i));
-            i++;
+        System.out.println("사용자가 입력한 값: " + number);
+
+        if (number < 2) {
+            System.out.println("입력한 값을 잘못 입력하였습니다.");
         }
-
-        number = 7;
-
-        //7단
-        for (i = 1; i < 10; i++) {
-            System.out.println(number + " * " + i + " = " + (number * i));
+        else if (number > 9) {
+            System.out.println("입력한 값을 잘못 입력하였습니다.");
+        }
+        else {
+            for (int i = 1; i < 10; i++) {
+                System.out.println(number + " * " + i + " = " + (number * i));
+            }
         }
     }
 }
